d7e00cd2603452c8e80a9c078b64adb971c9af1c
Bz-zik <narciss95@yandex.ru>
2017-11-30 23:35:39 +0300

    create project with base class and interface

---
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,15 @@
+package quoters;
+
+public class TerminatorQuoter implements Quoter {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        System.out.println("I'll be back!");
+    }
+}
