de25028e14978d0b3ef2dbfc91e6e8394471499d
Bz-zik <narciss95@yandex.ru>
2017-12-01 02:12:35 +0300

    create PostProxy annotation

---
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -0,0 +1,22 @@
+package quoters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private ConfigurableListableBeanFactory factory;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext context = event.getApplicationContext();
+        String[] names = context.getBeanDefinitionNames();
+        for (String name : names) {
+
+        }
+    }
+}
